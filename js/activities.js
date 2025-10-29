27694b6383c78751afe0da9815d3fe33ff9d03cc
Megan Sun <meganws@uci.edu>
2025-10-29 02:46:12 -0700

    Created the graph for the number of tweets per activity.

--- js/activities.js
+++ js/activities.js
@@ -86,18 +86,27 @@ function parseTweets(runkeeper_tweets) {
 
 	//TODO: create a new array or manipulate tweet_array to create a graph of the number of tweets containing each type of activity.
 
+	const activityCounts = Array.from(activityTypes.entries())
+	.map(([activity, data]) => ({ activity, count: data.totalCount }));
+
 	activity_vis_spec = {
-	  "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
-	  "description": "A graph of the number of Tweets containing each type of activity.",
-	  "data": {
-	    "values": tweet_array
-	  }
-	  //TODO: Add mark and encoding
+		"$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+	  	"description": "A graph of the number of Tweets containing each type of activity.",
+	  	"data": {
+			"values": activityCounts
+			//TODO: Add mark and encoding
+		},
+		"mark": "bar",
+		"encoding": {
+			"x": {"field": "activity", "type": "nominal", "title": "Activity Type"},
+    		"y": {"field": "count", "type": "quantitative", "title": "Number of Tweets"}
+		}
 	};
 	vegaEmbed('#activityVis', activity_vis_spec, {actions:false});
 
 	//TODO: create the visualizations which group the three most-tweeted activities by the day of the week.
 	//Use those visualizations to answer the questions about which activities tended to be longest and when.
+
 }
 
 //Wait for the DOM to load
